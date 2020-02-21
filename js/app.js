20d2d53692ab16e5016fc7cee627eb752fef2751
Michael Kline <klinem@gmail.com>
2020-02-20 20:21:04 -0700

    final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-
-
 const nav = document.querySelector('.nav');
+const ham = document.querySelector('.ham-wrap');
 
-
-document.querySelector('#accordion').addEventListener("click", () => (
-  nav.classList.toggle("open")
-))
+document.querySelector('#accordion').addEventListener("click", () => {
+  nav.classList.toggle("open");
+  ham.classList.toggle('open-ham');
+})
 
